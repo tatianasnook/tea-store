4a6b81bdfe11f1430c4c41df8251337f4d0f264d
tatianasnook <tatsnook@gmail.com>
2024-01-01 10:38:17 -0600

    data and images

--- src/data.js
+++ src/data.js
@@ -42,122 +42,122 @@ export const data = [
     },
     {
       id: 6,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'English Breakfast Tea',
+      searchTerm: 'black tea',
+      price: 4.25,
+      image: './images/6.jpg',
+      description: "English Breakfast, a robust black tea blend, complements the traditional English morning meal. The initial brew featured black Keemun tea sourced from China's Anhui province. Over time, various versions of this renowned blend have emerged. Our rendition of English Breakfast adheres closely to the classic recipe, crafted from premium black Keemun tea renowned for its rich, subtly smoky taste and precisely balanced astringency. It boasts a harmonious, distinctly honeyed aroma and can be savored either on its own or with a touch of milk."
     },
     {
       id: 7,
-      name: '',
-      searchTerm: 'green tea',
+      name: 'Black Dragon Pearl',
+      searchTerm: 'black tea',
       price: 5.71,
-      image: './images/',
-      description: " "
+      image: './images/7.jpg',
+      description: "Black Dragon Pearls, an exquisite hand-rolled black tea, presents a velvety, indulgent, and chocolate-infused flavor profile. Originating from the Yunnan province, this variation of the beloved Dragon Pearl is meticulously crafted from top-tier leaves and buds, artfully rolled into sizable pearl-like formations. The Black Dragon Pearls boast inherent sweetness and a seamless texture, accented by a subtle earthy undertone. Delicate whispers of cocoa unfold with each pearl, offering a transcendent tea experience that should not be overlooked. For optimal flavor, we recommend using 2-3 pearls per cup."
     },
     {
       id: 8,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Earl Grey Lavender',
+      searchTerm: 'black tea',
+      price: 4.75,
+      image: './images/8.jpg',
+      description: "Enhanced by the infusion of lavender flowers, our Earl Grey Lavender draws upon the rich historical legacy of these blooms, renowned for their medicinal, culinary, and household applications. The sweet, floral aroma of lavender imparts a soothing quality, evoking a sense of relaxation. When harmoniously paired with the traditional citrus notes of Earl Grey and a subtle hint of cream, this blend is destined to become a cherished companion during moments of tranquility."
     },
     {
       id: 9,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Golden Monkey Tea',
+      searchTerm: 'black tea',
+      price: 4.55,
+      image: './images/9.jpg',
+      description: "Renowned as Jin Hou or Golden Monkey, this distinguished Chinese black tea is aptly named for its leaves' resemblance to monkey paws. Hailing from the Fujian province, the Golden Monkey undergoes meticulous hand-processing each spring, with the careful selection of one leaf and one bud. This particular variety exhibits a delightful and intricate flavor profile, offering sweetness and a myriad of savory elements. Expect notes of roasted apples, palm sugar, walnut, cocoa, rye, and subtle spices that linger with each sip. The tea unfolds with a rich, coating texture and a remarkably smooth, soft mouthfeel, indicative of its high quality. Golden Monkey proves to be a truly complex black tea, perfect for multiple infusions, making it a delightful indulgence for aficionados of black tea."
     },
     {
       id: 10,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Vanilla Bean Tea',
+      searchTerm: 'black tea',
+      price: 3.75,
+      image: './images/10.lpg',
+      description: "Crafted from a rich fusion of black tea leaves and intoxicating vanilla, our Vanilla Bean black tea evokes the sensory experience of breathing in the lush fragrance of wild vanilla during a tropical hike. The profound, malty depths of Indian and Ceylon black tea are enfolded in the sweet and sticky aromatic oils, a unique contribution from authentic and fragrant vanilla beans. The result is a cup of tea that is not only heady and aromatic but also transports you to a captivating realm of flavor."
     },
     {
       id: 11,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Blood Orange Tea',
+      searchTerm: 'herbal tea',
+      price: 4.75,
+      image: './images/11.jpg',
+      description: "Celebrated for its intense vibrant hue and a delightful sweet-tangy taste, the blood orange stands out as a culinary gem. It becomes a key element in our refreshing herbal blend. The Blood Orange tea expertly combines blood orange peels with hibiscus flowers and rose hips, resulting in a naturally sweet, caffeine-free infusion. This tea boasts a rich, hearty, and tangy profile, striking a harmonious balance between the dryness of orange peel and the succulence of fruit juices."
     },
     {
       id: 12,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Lemongrass Tea',
+      searchTerm: 'herbal tea',
+      price: 4.95,
+      image: './images/12.jpg',
+      description: "Originating from Southeast Asia, lemongrass made its debut in the American culinary scene as Thai cuisine gained popularity. Boasting a delicate Meyer lemon flavor with a subtle touch of sweet ginger, this herb exudes a highly aromatic floral fragrance. When steeped as a straightforward cup of tea, lemongrass offers a smooth and fragrant experience, featuring a soft mouthfeel and a clean finish. Remarkably uplifting, this herbal infusion is naturally caffeine-free."
     },
     {
       id: 13,
-      name: '',
-      searchTerm: 'green tea',
+      name: 'Beach Bellini',
+      searchTerm: 'herbal tea',
       price: 5.71,
-      image: './images/',
-      description: " "
+      image: './images13.jpg',
+      description: "Get ready to embrace the tropical vibes with this vibrant herbal tea! Combining the invigorating and zesty notes of peach, strawberries, mango, pineapple, and papaya, this sunny blend transports you to an exotic island getaway. The enticing aroma alone will evoke the sensation of lounging in a far-off paradise, without the need for a ten-hour plane ride. Pour this freshly steeped tea over ice for a refreshing treat, and you'll feel like you've captured the essence of summertime in a cup! Add a tiny pink umbrella as the finishing touch, and you're all set for a tropical escape."
     },
     {
       id: 14,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Spearmint Tea',
+      searchTerm: 'herbal tea',
+      price: 4.95,
+      image: './images/14.jpg',
+      description: "Hailing from the Mediterranean, spearmint was once revered by ancient Romans for its reputation as a mind-stirring herb. Today, it remains cherished for its invigoratingly clear aroma and calming properties. If peppermint feels a bit too intense at times, then spearmint is the perfect choice. Offering a rounded, slightly smoother, and more grounded taste compared to peppermint, it combines warming and cooling notes seamlessly. Remarkably gentle on the palate, this herb is naturally caffeine-free, providing a soothing and enjoyable experience."
     },
     {
       id: 15,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Mango Melange Tea',
+      searchTerm: 'herbal tea',
+      price: 5.25,
+      image: './images/15.jpg',
+      description: "Experience the delightful fusion of fresh mango pieces, apple, and hibiscus flowers in this tangy and fruity tropical blend. Immerse yourself in a deep, rosy red cup with a juicy texture and a sweet, soft mango finish. This refreshing infusion is entirely caffeine-free and contains no sugar or other sweeteners. As the flavors intertwine, it's a perfect call to embrace the essence of summertime in every sip."
     },
     {
       id: 16,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'White Grapefruit Tea',
+      searchTerm: 'white tea',
+      price: 4.75,
+      image: './images/16.jpg',
+      description: "Combining tangy grapefruit with naturally sweet white tea from China, this exquisite blend delivers a delightfully sour-sweet tasting cup. Infused with a zesty citrus flavor, it culminates in a slightly dry and refreshing finish. Whether enjoyed hot or iced, this tea is a perfect choice any time of the day."
     },
     {
       id: 17,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'White Strawberry Tea',
+      searchTerm: 'white tea',
+      price: 4.55,
+      image: './images/17.jpg',
+      description: "Crafted on a foundation of white tea from China featuring inherent melon notes, this blend pays homage to the splendid strawberry. Renowned for lifting spirits and pleasing palates, strawberries take center stage in this intricate composition. With strawberry overtones and a subtle hint of vanilla, this blend exquisitely crowns the Queen of Berries."
     },
     {
       id: 18,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Jasmine Silver Needle',
+      searchTerm: 'white tea',
+      price: 4.95,
+      image: './images/18.jpg',
+      description: "Featuring young and elegant tea leaf buds, Jasmine Silver Needle white tea carries a subtle hint of jasmine aura. It encapsulates all the allure and softly sweet nuances of a classic Silver Needle tea, elevated by the graceful aroma of night-blooming jasmine flowers. With an airy flavor, delicate mouthfeel, and a pleasantly dry finish, this tea embodies tranquility. Its quiet nature makes it the perfect gentle companion for your special moments."
     },
     {
       id: 19,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'White Tropical Tea',
+      searchTerm: 'white tea',
+      price: 5.75,
+      image: './images/19.jpg',
+      description: "Embark on a journey to paradise with our exquisite White Tropical blend. Crafted from naturally sweet white tea from China, it harmoniously intertwines with the luscious flavors of coconuts and pineapple. Revel in the rounded mouthfeel and the delightfully simple, sweet finish that soothes from start to finish. Whether enjoyed hot or iced, this unique blend has the power to transport you to a sunny beach even on the dreariest, coldest day. Just don't forget your sunblock!"
     },
     {
       id: 20,
-      name: '',
-      searchTerm: 'green tea',
-      price: 5.71,
-      image: './images/',
-      description: " "
+      name: 'Pineapple Youthberry Tea',
+      searchTerm: 'white tea',
+      price: 4.95,
+      image: './images/20.jpg',
+      description: "Radiating youthfulness and playfulness, this tantalizing white tea blend showcases candied pineapple, luscious mango, and sweet berries. Crafted from tender young tea leaves and buds, this white tea is not only succulent but also packed with anti-aging properties and L-theanine, a rare amino acid known for its potential to alleviate stress and enhance mood. Indulge in this fruity, tropical treat – a delightful option for those with a sweet tooth!"
     }
   ]
